36a78f44febec05a2cc7a65a4aebb258310eb461
Yauheniya Mastavaya <jk@db.by>
2023-05-01 15:48:49 +0300

    fix: change pathes

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,4 +1,4 @@
-import { state } from './../../state.js';
+import { state } from './../state.js';
 import {
   changeStateOnButtonClick,
   changeStateOnMousedown,
--- components/Screen/index.js
+++ components/Screen/index.js
@@ -1,4 +1,4 @@
-import { state } from './../../state.js';
+import { state } from './../state.js';
 import { getCharacter } from '../../utils/utils.js';
 
 export class Screen {
